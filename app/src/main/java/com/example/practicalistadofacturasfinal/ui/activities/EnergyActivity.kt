3da98da5055614c08905927e85efd99d1453effd
6002524 <jrgaln.m@gmail.com>
2024-04-08 14:40:55 +0200

    Dimensiones aplicadas a EnergyActivity.kt.

--- app/src/main/java/com/example/practicalistadofacturasfinal/ui/activities/EnergyActivity.kt
+++ app/src/main/java/com/example/practicalistadofacturasfinal/ui/activities/EnergyActivity.kt
@@ -6,15 +6,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.practicalistadofacturasfinal.R
+import com.example.practicalistadofacturasfinal.databinding.ActivityEnergyBinding
 
 class EnergyActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEnergyBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_energy)
+        binding = ActivityEnergyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setInsets()
+    }
+
+    private fun setInsets() {
+        val padding = resources.getDimension(R.dimen.activities_fragments_padding).toInt()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left + padding, systemBars.top + padding, systemBars.right + padding, systemBars.bottom + padding)
             insets
         }
     }
