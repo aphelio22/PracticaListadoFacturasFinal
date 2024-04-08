f74a6bc0a0f9b100b9e1ef0f04d25918222d300c
6002524 <jrgaln.m@gmail.com>
2024-04-08 14:37:35 +0200

    Dimensiones aplicadas a SignUpActivity.kt.

--- app/src/main/java/com/example/practicalistadofacturasfinal/ui/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/practicalistadofacturasfinal/ui/activities/SignUpActivity.kt
@@ -6,15 +6,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.practicalistadofacturasfinal.R
+import com.example.practicalistadofacturasfinal.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setInsets()
+    }
+
+    private fun setInsets() {
+        val padding = resources.getDimension(R.dimen.activities_fragments_padding).toInt()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left + padding, systemBars.top + padding, systemBars.right + padding, systemBars.bottom + padding)
             insets
         }
     }
