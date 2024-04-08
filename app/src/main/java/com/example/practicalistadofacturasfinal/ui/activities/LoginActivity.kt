b5d04ec04695c1376842676161d25a7a24b81e69
6002524 <jrgaln.m@gmail.com>
2024-04-08 14:34:14 +0200

    Dimensiones del padding añadidas en el archivo dimens.xml para las distintas actividades y los distintos fragments. Dimensiones aplicadas a LoginActivity.kt.

--- app/src/main/java/com/example/practicalistadofacturasfinal/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/example/practicalistadofacturasfinal/ui/activities/LoginActivity.kt
@@ -1,22 +1,39 @@
 package com.example.practicalistadofacturasfinal.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.practicalistadofacturasfinal.R
+import com.example.practicalistadofacturasfinal.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setInsets()
+        binding.btLogin.setOnClickListener {
+            val miIntent = Intent(this, SelectionActivityM::class.java)
+            startActivity(miIntent)
+        }
+    }
+
+    private fun setInsets() {
+        val padding = resources.getDimension(R.dimen.activities_fragments_padding).toInt()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(
+                systemBars.left + padding,
+                systemBars.top + padding,
+                systemBars.right + padding,
+                systemBars.bottom + padding
+            )
             insets
         }
-        
     }
 }
